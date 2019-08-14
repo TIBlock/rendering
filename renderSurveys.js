215162870c355b793db4d698f63758b0c7482f57
Taylor Blocker <taylorblocker@gmail.com>
2019-08-13 21:30:41 -0500

    started the render surveys exercise, instead of using for this time I am using foreach to see if I can tighten it up. first successfully question output was a pain..

--- renderSurveys.js
+++ renderSurveys.js
@@ -1,11 +1,61 @@
 
-function renderSurveys(surveys) {
+function renderSurveys(surveys) 
+{
+    let finalSurvey = surveys.map(surveyContent).join("")
+
     return `
         <div class="text-center mt-5">
-            <code>${JSON.stringify(surveys)}</code>
+            ${finalSurvey}
         </div>
     `
 }
+
+
+
+function surveyContent(surveys)
+{
+    let question = "";
+    surveys.fields.forEach(element => {
+        question += `
+        <div class="survey_question">${element.label}</div><br>
+        `;
+        return question;
+    });
+
+
+    //May need to nest survey items in above function like albums.
+    
+    let surveyItems = "";
+
+    surveys.fields.
+
+    return `
+        <div>
+            <div class="survey_title">${surveys.title}</div>
+            <div class="survey_question">${question}</div>
+                <div class="button_container">
+                </div>
+        </div>
+    `
+    // function askQuestion(surveys) {
+        
+        // }
+        
+}
+        // console.dir(surveys.fields[0].label)
+
+
+    // write an if statement that can tell what type of content the survey needs and add each element to the total survey
+// IE you can have a bunch of buttons, or a bunch of texts etc, just depends on what value is requested in the data. 
+
+//Start by just getting the titles of the sureys to show then down from there. 
+
+
+
+// function renderButtons(surveys)
+
+
+
 
 function surveys() {
     var content = document.getElementById('content');
